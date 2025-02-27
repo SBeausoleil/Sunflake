0d33f5a8f43a1d56774a0558c40e1a4bea05a0ec
Samuel Beausoleil <samuel.beausoleil@hotmail.com>
2025-02-27 14:08:28 -0500

    Epoch configuration

--- hibernate/src/main/java/com/sb/flake/SunflakeConfiguration.java
+++ hibernate/src/main/java/com/sb/flake/SunflakeConfiguration.java
@@ -8,15 +8,19 @@ import systems.helius.commons.SmartProperties;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.DateTimeException;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeParseException;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Global default configuration for the Sunflake module.
  */
-public class SunflakeConfiguration {
+public final class SunflakeConfiguration {
     public static final String FILE_NAME = "sunflake.properties";
-    private static final Logger log = LoggerFactory.getLogger(SunflakeConfiguration.class);
     private static final String PREFIX = "sunflake.";
     public static final String PRESET = PREFIX + "preset";
     public static final String PER_TABLE_GENERATOR = PREFIX + "perTableGenerator";
@@ -29,18 +33,19 @@ public class SunflakeConfiguration {
     public static final String TIMESTAMP_UNIT = TIMESTAMP + "unit";
     public static final String TIMESTAMP_UNITS_PER_TICK = TIMESTAMP_UNIT + "unitsPerTick";
     public static final String TIMESTAMP_ALLOW_USAGE_OF_SIGN_BIT = TIMESTAMP_UNIT + "allowUsageOfSignBit";
+    public static final String EPOCH_PROPERTY = PREFIX + "epoch";
 
     private static GenerationRules globalRules = null;
-    private static LocalDate epoch;
+    private static Instant epoch = null;
+
+    private SunflakeConfiguration() {}
 
     /**
      * Return the global rules.
-     * <p>
-     *     If they are not already set, the sunflake.properties file will be read.
-     * </p>
+     *
      * @return the global rules.
      */
-    public static GenerationRules getGlobalRules() {
+    public static synchronized GenerationRules getGlobalRules() {
         if (globalRules == null) {
             initialize();
         }
@@ -48,33 +53,51 @@ public class SunflakeConfiguration {
     }
 
     /**
-     * Set the rules that all new SunflakeProvider used by Hibernate will use.
-     * @param globalRules
+     * Return the global epoch.
+     *
+     * @return the global epoch.
      */
-    public static synchronized void setGlobalRules(GenerationRules globalRules) {
-        globalRules = globalRules;
+    public static synchronized Instant getEpoch() {
+        if (epoch == null) {
+            initialize();
+        }
+        return epoch;
     }
 
     private static synchronized void initialize() {
-        if (globalRules != null) return;
+        if (globalRules != null && epoch != null) return;
 
+        SmartProperties properties = readProperties();
         try {
-            SmartProperties properties = readProperties();
-            FlakePreset preset = properties.getEnum(PRESET, FlakePreset.class);
-            if (preset != null) {
-                globalRules = preset.getRules();
-                return;
+            if (globalRules == null) {
+                Optional<FlakePreset> preset = properties.getEnum(PRESET, FlakePreset.class);
+                globalRules = preset.map(FlakePreset::getRules)
+                        .orElseGet(() -> {
+                            var rules = new GenerationRulesBuilder();
+                            properties.ifIntPresent(SEQUENCE_SIZE, rules::setSequenceSize)
+                                    .ifIntPresent(WORKER_ID_SIZE, rules::setWorkerIdSize)
+                                    .ifIntPresent(TIMESTAMP_SIZE, rules::setTimestampSize)
+                                    .ifBooleanPresent(TIMESTAMP_ALLOW_USAGE_OF_SIGN_BIT, rules::setAllowUsageOfSignBit)
+                                    .ifEnumPresent(TIMESTAMP_UNIT, TimeUnit.class, rules::setTimeUnit);
+                            return rules.build();
+                        });
             }
-
-            var rules = new GenerationRulesBuilder();
-            properties.ifIntPresent(SEQUENCE_SIZE, rules::setSequenceSize)
-                    .ifIntPresent(WORKER_ID_SIZE, rules::setWorkerIdSize)
-                    .ifIntPresent(TIMESTAMP_SIZE, rules::setTimestampSize)
-                    .ifBooleanPresent(TIMESTAMP_ALLOW_USAGE_OF_SIGN_BIT, rules::setAllowUsageOfSignBit)
-                    .ifEnumPresent(TIMESTAMP_UNIT, TimeUnit.class, rules::setTimeUnit);
-            globalRules = rules.build();
         } catch (IllegalArgumentException e) {
             throw new InitializationException(e);
+        }
+        if (epoch == null) {
+            String epochProp = properties.getProperty(EPOCH_PROPERTY);
+            if (epochProp != null) {
+                try {
+                    epoch = LocalDate.parse(epochProp)
+                            .atStartOfDay()
+                            .toInstant(ZoneOffset.UTC);
+                } catch (DateTimeParseException e) {
+                    throw new InitializationException("Epoch property is incorrectly formatted. Make sure it follows the yyyy-MM-dd format.");
+                }
+            } else {
+                throw new InitializationException("Epoch property is not set. Set an epoch for the property " + EPOCH_PROPERTY + " in the Sunflake configuration file.");
+            }
         }
     }
 
