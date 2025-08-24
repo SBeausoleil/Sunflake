bc5cbed379d541ea8c903dc6b0cf47999ce10e29
Samuel Beausoleil <sbeausoleil_dev@hotmail.com>
2025-08-24 15:30:57 -0400

    Test Application

---
+++ TestApp/src/main/java/edu/sbeausoleil/log121/testapp/TestAppApplication.java
@@ -0,0 +1,13 @@
+package edu.sbeausoleil.log121.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAppApplication.class, args);
+    }
+
+}
---
+++ TestApp/src/test/java/edu/sbeausoleil/log121/testapp/TestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sbeausoleil.log121.testapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
